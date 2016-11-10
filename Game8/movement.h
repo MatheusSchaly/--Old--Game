f19daf58a4d0dc670e7a0e00661a6f7c24589d56
Matheus Schaly <matheusmhs@hotmail.com>
2016-11-10 18:08:02 -0200

    Adjusting Map's Array

---
+++ Game8/movement.h
@@ -0,0 +1,8 @@
+#ifndef MOVEMENT_H
+#define MOVEMENT_H
+
+std::string checkDirection (); // Checks character's direction.
+int movingVertically (std::string direction, int i, int j); // Moves the character vertically.
+int movingHorizontally (std::string direction, int i, int j); // Moves the character horizontally.
+
+#endif
