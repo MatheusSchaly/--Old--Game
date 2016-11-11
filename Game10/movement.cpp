25e0e1f9b5ff16bf16e79e030b07ff7c0ef0399d
Matheus Schaly <matheusmhs@hotmail.com>
2016-11-11 18:49:44 -0200

    Creating Foes

---
+++ Game10/movement.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+#include "movement.h"
+
+// Checks character's direction:
+
+std::string checkDirection () {
+
+    std::string direction;
+
+    do {
+        std::cout << "\nWhich direction do you want to move? (up / down / left / right)\n";
+        std::cin >> direction;
+        std::transform(direction.begin(), direction.end(), direction.begin(), ::tolower);
+    } while (direction != "up" && direction != "down" && direction != "left" & direction != "right");
+
+    return direction;
+
+}
+
+// Moves the character vertiaclly:
+
+int movingVertically (std::string direction, int i) {
+
+    if (direction == "up") {
+        i--;
+    } else {
+        i++;
+    }
+
+    return i;
+
+}
+
+// Moves the character horizontally:
+
+int movingHorizontally (std::string direction, int j) {
+
+    if (direction == "left") {
+        j--;
+    } else {
+        j++;
+    }
+
+    return j;
+
+}
+
---
+++ Game10/movement.h
@@ -0,0 +1,8 @@
+#ifndef MOVEMENT_H
+#define MOVEMENT_H
+
+std::string checkDirection (); // Checks character's direction.
+int movingVertically (std::string direction, int i); // Moves the character vertically.
+int movingHorizontally (std::string direction, int j); // Moves the character horizontally.
+
+#endif
