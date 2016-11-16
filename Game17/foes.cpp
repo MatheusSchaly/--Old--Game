b8be1b92b0e60988f0565c10948dfc5251028472
Matheus Schaly <matheusmhs@hotmail.com>
2016-11-16 20:57:16 -0200

    Adjusting Foes

---
+++ Game17/foes.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+#include "foes.h"
+#include "general.h"
+
+// Shows the map's messages, creates the foes' interactions:
+
+std::string territoryMessage (int foeNumber, int territory, int &life) {
+
+    std::string mapMessage, empty, fightOrFlee, giantAction;
+    int giantLife;
+
+    giantLife = 100;
+    empty = "there is no one there.";
+
+    switch (territory) {
+
+        case 3:
+            switch (foeNumber) {
+                case 1:
+                    mapMessage = empty;
+                    break;
+                case 2:
+                    mapMessage = "you see a furies giant.";
+                    fightOrFlee = checkTwoWords ("fight", "flee");
+                    if (fightOrFlee == "fight") {
+                        do {
+                            giantAction = randomizeUpToFiveWords ("He threw you seven meters away, and you hit a tree", "He punched you right in your face", "0", "0", "0");
+                            if (giantAction == "He threw you seven meters away, and you hit a tree") {
+                                damage ()
+                            }
+                        } while (giantLife > 0);
+                    }
+                    break;
+                case 3:
+                    mapMessage = "you came into an ambush.";
+                    break;
+                default:
+                    mapMessage = "there are sleepy monkeys in the trees around you.";
+                    break;
+            }
+
+        case 4:
+            switch (foeNumber) {
+                case 1:
+                    mapMessage = empty;
+                    break;
+                case 2:
+                    mapMessage = "you see a troll.";
+                    break;
+                case 3:
+                    mapMessage = "you see a group of people.";
+                    break;
+                default:
+                    mapMessage = "there are wild bulls all around the field.";
+                    break;
+            }
+
+        default:
+            switch (foeNumber) {
+                case 1:
+                    mapMessage = empty;
+                    break;
+                case 2:
+                    mapMessage = "you see an orc.";
+                    break;
+                case 3:
+                    mapMessage = "you see a wagon coming at your direction.";
+                    break;
+                default:
+                    mapMessage = "there are sheep blocking the road.";
+                    break;
+            }
+    }
+
+    return mapMessage;
+
+}
---
+++ Game17/foes.h
@@ -0,0 +1,8 @@
+#ifndef FOES_H
+#define FOES_H
+
+std::string territoryMessage (int foeNumber, int territory, int life); // Shows the map's message.
+std::string chooseFightOrFlee (); // Choosing between fight or flee.
+std::string giantActionGenerator (int giantActionsQuantity);
+
+#endif
