cd6e58ccd3eaa93a6a06f8d04c0782ed224d134f
Matheus Schaly <matheusmhs@hotmail.com>
2016-11-17 18:45:16 -0200

    Simplifying Functions

---
+++ Game20/map.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include "map.h"
+
+// Show's the map's place:
+
+std::string mapPlace (int n) {
+
+    std::string terrain;
+
+    switch (n) {
+        case 0:
+            terrain = "the sea";
+            break;
+        case 1:
+            terrain = "lake";
+            break;
+        case 2:
+            terrain = "hills";
+            break;
+        case 3:
+            terrain = "a forest";
+            break;
+        case 4:
+            terrain = "a open field";
+            break;
+        default:
+            terrain = "a road";
+            break;
+    }
+
+    return terrain;
+
+}
+
